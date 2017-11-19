25bd8e037be7de1a8c5129f1b6d122b618e1b725
briantomasco <brian.a.tomasco.20@dartmouth.edu>
2017-11-19 15:04:06 -0500

    Added email field and password check to create account activity.

--- app/src/main/java/com/apptime/alexw/dartmouthdrinks/Create_Acct_Activity.java
+++ app/src/main/java/com/apptime/alexw/dartmouthdrinks/Create_Acct_Activity.java
@@ -3,33 +3,46 @@ package com.apptime.alexw.dartmouthdrinks;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by zacharyjohnson on 11/18/17.
  */
 
-public class Create_Acct_Activity extends Activity{
+public class Create_Acct_Activity extends AppCompatActivity {
 
-    EditText name = findViewById(R.id.create_acct_name);
-    EditText pw = findViewById(R.id.create_acct_password);
-    EditText confirm = findViewById(R.id.reconfirm_password);
+    EditText name;
+    EditText email;
+    EditText pw;
+    EditText confirm;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acct);
+        name = findViewById(R.id.create_acct_name);
+        email = findViewById(R.id.create_acct_email);
+        pw = findViewById(R.id.create_acct_password);
+        confirm = findViewById(R.id.reconfirm_password);
     }
 
     public void createOnClearClick(View v) {
         name.setText("");
+        email.setText("");
         pw.setText("");
         confirm.setText("");
     }
 
     public void onNextClick(View v) {
         Intent next = new Intent("NEXT");
-        startActivity(next);
+        if (!pw.getText().equals(confirm.getText())){
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            startActivity(next);
+        }
     }
 
     public void onSignInClick(View v) {
