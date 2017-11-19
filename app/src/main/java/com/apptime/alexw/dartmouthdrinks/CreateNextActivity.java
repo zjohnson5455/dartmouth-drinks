d555d868fb201f030c77e862e34ea4a98ba70590
Alex Waterhouse <ajw.20@dartmouth.edu>
2017-11-19 16:51:06 -0500

    Hopefully got sign-in and sign-up working

--- app/src/main/java/com/apptime/alexw/dartmouthdrinks/CreateNextActivity.java
+++ app/src/main/java/com/apptime/alexw/dartmouthdrinks/CreateNextActivity.java
@@ -5,7 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by zacharyjohnson on 11/18/17.
@@ -13,32 +20,62 @@ import android.widget.Spinner;
 
 public class CreateNextActivity extends Activity {
 
-    Spinner sex = findViewById(R.id.create_acct_sex);
+    Spinner mSexSpinner;
+    EditText mNameEditText;
+    EditText mWeightEditText;
+
+    DatabaseReference mDatabase;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_next);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.Sex, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+
+        mSexSpinner = findViewById(R.id.create_acct_sex);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Sex, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        sex.setAdapter(adapter);
+        // Apply the adapter to the spinner
+        mSexSpinner.setAdapter(adapter);
+
+        mNameEditText = findViewById(R.id.nameEditText);
+        mWeightEditText = findViewById(R.id.weightEditText);
+
+
     }
 
     public void onCreateClick(View v) {
         //TODO fix intents
-        Intent create = new Intent("");
-        startActivity(create);
+
+        if (mNameEditText.getText().toString().length() != 0 && mWeightEditText.getText().toString().length() != 0 && !mSexSpinner.getSelectedItem().toString().equals("---")){
+            //do a thing: user initialise
+
+            //User user = new User()
+            //mDatabase.child("users").child(currentUser.getUid()).setValue(user);
+            Intent add = new Intent(getApplicationContext(), AddActivity.class);
+            startActivity(add);
+            finish();
+        }
+        else if (mNameEditText.getText().toString().length() == 0){
+            Toast.makeText(getApplicationContext(), "Enter a name, we want to know who you are!", Toast.LENGTH_SHORT);
+        }
+        else if (mWeightEditText.getText().toString().length() == 0){
+            Toast.makeText(getApplicationContext(), "Enter a weight, we won't tell!", Toast.LENGTH_SHORT);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Enter a sex!", Toast.LENGTH_SHORT);
+        }
+
+
+
+
     }
 
-    public void onSignInClick(View v) {
-        Intent sign = new Intent("SIGN");
-        startActivity(sign);
-    }
-
-    public void onBackClick(View v) {
-        Intent back = new Intent("CREATE");
-        startActivity(back);
-    }
 }
--- app/src/main/java/com/apptime/alexw/dartmouthdrinks/SignInActivity.java
+++ app/src/main/java/com/apptime/alexw/dartmouthdrinks/SignInActivity.java
@@ -3,10 +3,17 @@ package com.apptime.alexw.dartmouthdrinks;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by zacharyjohnson on 11/18/17.
@@ -14,20 +21,52 @@ import android.widget.Toast;
 
 public class SignInActivity extends Activity {
 
-    EditText name = findViewById(R.id.sign_in_name);
-    EditText pw = findViewById(R.id.sign_in_password);
+    final static String TAG = "SignInActivity";
+    EditText mEmailEditText;
+    EditText mPasswordEditText;
+    private FirebaseAuth mAuth;
 
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+        mEmailEditText = findViewById(R.id.sign_in_email);
+        mPasswordEditText = findViewById(R.id.sign_in_password);
+
+        if (mAuth.getCurrentUser() != null){
+            Intent add = new Intent(getApplicationContext(), AddActivity.class);
+            startActivity(add);
+            finish();
+        }
+
+
 
     }
 
     public void onSignInClick(View v) {
-        Intent add = new Intent(getApplicationContext(), AddActivity.class);
-        startActivity(add);
-        finish();
+
+        mAuth.signInWithEmailAndPassword(mEmailEditText.getText().toString(), mPasswordEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Intent add = new Intent(getApplicationContext(), AddActivity.class);
+                            startActivity(add);
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
     }
 
     public void onCreateClick(View v) {
@@ -37,7 +76,7 @@ public class SignInActivity extends Activity {
     }
 
     public void signInOnClearClick(View v) {
-        name.setText("");
-        pw.setText("");
+        mEmailEditText.setText("");
+        mPasswordEditText.setText("");
     }
 }
