f2b5c17183cd22e8fadcef8e70276edcdd40ddd1
Alex Waterhouse <ajw.20@dartmouth.edu>
2017-11-20 02:01:58 -0500

    Some work on settings

--- app/src/main/java/com/apptime/alexw/dartmouthdrinks/SettingsActivity.java
+++ app/src/main/java/com/apptime/alexw/dartmouthdrinks/SettingsActivity.java
@@ -6,16 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    DatabaseReference databaseUser;
+    User currentTimeUser;
 
     Context mContext;
 
     Button mSignOutButton;
+    TextView mNameTextView;
+    TextView mEmailTextView;
+    TextView mWeightTextView;
+    TextView mSexTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,39 @@ public class SettingsActivity extends AppCompatActivity {
 
         mContext = this;
         mAuth = FirebaseAuth.getInstance();
+
+        mNameTextView = findViewById(R.id.nameTextView);
+        mEmailTextView = findViewById(R.id.emailTextView);
+        mWeightTextView = findViewById(R.id.weightTextView);
+        mSexTextView = findViewById(R.id.sexTextView);
+
+
+
+        mDatabase = Utils.getDatabase().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        databaseUser = Utils.getDatabase().getReference("users").child(currentUser.getUid());
+
+        databaseUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                currentTimeUser = dataSnapshot.getValue(User.class);
+                mNameTextView.setText(currentTimeUser.getName().toString());
+                mEmailTextView.setText(currentUser.getEmail().toString());
+                mWeightTextView.setText(String.valueOf(currentTimeUser.getWeight()) + " lbs");
+                if (currentTimeUser.isMale()) mSexTextView.setText("Male");
+                else mSexTextView.setText("Female");
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+
 
 
         mSignOutButton = findViewById(R.id.sign_out_button);
